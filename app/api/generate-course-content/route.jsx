2c2a4c198bf1bdb8eaf1c080d96bdb15f68f0904
Sujeeth <Sujeethkumararjun@gmail.com>
2025-07-13 21:55:20 +0530

    Integrated Youtube API + AI LLM Model (Mishral META) trained using google lmbook for youtube purposes

--- app/api/generate-course-content/route.jsx
+++ app/api/generate-course-content/route.jsx
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { ai } from "../generate-course-layout/route";
+import axios from "axios";
 
 const PROMPT = `Depends on Chapter name and Topic Generate content for each topic in HTML and give response in JSON format (Avoid without numbering or metadata like [5]).
 Schema:{
@@ -15,7 +16,7 @@ Schema:{
 export async function POST(req) {
   const { courseJson, courseTitle, courseId } = await req.json();
 
-  const promises = courseJson?.chapters?.map(async(chapter) => {
+  const promises = courseJson?.chapters?.map(async (chapter) => {
     const config = {
       responseMimeType: "text/plain",
     };
@@ -36,19 +37,53 @@ export async function POST(req) {
       config,
       contents,
     });
-    console.log(response.candidates[0].content.parts[0].text);
+    // console.log(response.candidates[0].content.parts[0].text);
     const RawResp = response.candidates[0].content.parts[0].text;
-    const RawJson = RawResp.replace('```json', '').replace('```', '');
+    const RawJson = RawResp.replace("```json", "").replace("```", "");
     const JSONResp = JSON.parse(RawJson);
 
     //Get Youtube videos
-    return JSONResp;
+
+    const youtubeData = await GetYoutubeVideo(chapter?.chaptername);
+    console.log({
+      youtubeVideo: youtubeData,
+      CourseData: JSONResp,
+    })
+    return {
+      youtubeVideo: youtubeData,
+      CourseData: JSONResp,
+    };
   });
 
-  const CourseContent=await Promise.all(promises) 
+  const CourseContent = await Promise.all(promises);
 
   return NextResponse.json({
     courseName: courseTitle,
-    CourseContent: CourseContent
+    CourseContent: CourseContent,
   });
 }
+
+const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+const GetYoutubeVideo = async (topic) => {
+  const params = {
+    part: "snippet",
+    maxResults: 1,
+    q: topic,
+    type: "video",
+    key: process.env.YOUTUBE_API_KEY,
+  };
+  const resp = await axios.get(YOUTUBE_BASE_URL, { params });
+  const youtubeVideoListResp = resp.data.items;
+  const youtubeVideoList = [];
+  youtubeVideoListResp.forEach((item) => {
+    const data = {
+      videoId: item.id?.videoId,
+      title: item?.snippet?.title,
+      // description:item.snippet.description,
+      // thumbnail:item.snippet.thumbnails.default.url
+    };
+    youtubeVideoList.push(data);
+  });
+  console.log("youtubeVideoList", youtubeVideoList);
+  return youtubeVideoList;
+};
